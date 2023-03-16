232b0b029e6639fcd47868eb8ea010e06834711e
levimaciell <levimaciell1221@gmail.com>
2023-03-15 21:15:57 -0300

    Texto atv01: bug de caracteres e formatação

---
+++ Atv01/Code/aventura.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+//inputs
+//Nome da pessoa, nome de um amigo, idade da pessoa, idade de um amigo
+
+int main(){
+
+    int idadePessoa, idadeAmigo;
+    string nomePessoa, nomeAmigo;
+
+    cout << "Oi! bem vindos a aventura! Por favor, informe o seguinte para a sua aventura!" << endl;
+
+    cout << "Qual o seu nome?" << endl;
+    cin >> nomePessoa;
+
+    cout << "Informe o nome de um amigo ou outra pessoa" << endl;
+    cin >> nomeAmigo;
+
+    cout << "Agora, informe a idade dessa pessoa: ";
+    cin >> idadeAmigo;
+
+    cout << "Por fim, informe a sua idade: " << endl;
+    cin >> idadePessoa;
+
+    cout << "   Era uma vez um ser mágico conhecido como Majin Boo. Ele era conhecido por sua aparência peculiar, com pele rosa e um rosto gordinho, mas também era temido por sua imensa força e habilidades mágicas." << endl;
+
+    cout << "   Majin Boo passou anos aterrorizando o universo, destruindo planetas inteiros e espalhando o caos por onde quer que passasse. Ele era invencível e aparentemente indestrutível." << endl;
+
+    cout << "   No entanto, um dia, uma equipe de guerreiros corajosos decidiu enfrentar Majin Boo e impedir que ele causasse mais destruição. Liderados pelos lendários guerreiros Goku e Vegeta, os guerreiros mais poderosos do planeta se prepararam para a batalha final contra o temido vilão." << endl;
+
+    cout << "   A batalha foi épica e intensa, com Majin Boo usando suas habilidades mágicas de regeneração . Mas os guerreiros não desistiram, e lutaram com toda a sua força para tentar derrotar o monstro." << endl;
+
+    cout << "   Os guerreiros estavam exaustos e parecia que a batalha estava perdida. Majin boo era muito poderoso para os guerreiros da terra. Mas então, algo surpreendente aconteceu: Entraram no campo de batalha os guerreiros " << nomePessoa << " e " << nomeAmigo << " para enfrentar o temivel monstro rosa." << endl;
+
+    cout << "   Foi uma batalha difícil, mas majin boo estava levando a melhor sobre os dois poderosos guerreiros " << nomePessoa << " e " << nomeAmigo << ", eles não conseguiriam lutar por muito mais tempo." << endl;
+
+    //Mudar linha abaixo para o nome do personagem mais velho
+    cout << "   Então, em um ato de desespero, o guerreiro mais velho decide agarrar o monstro e usar toda sua energia para se auto-destruir e levar majin boo no processo. Quando a grande nuvem de poeira abaixou, o monstro não existia mais, assim como o guerreiro mais velho também foi destruido" << endl;
+
+    cout << "   Graças ao guerreiro mais velho, a terra viveria um futuro de paz e esperança!" << endl;
+
+
+}
