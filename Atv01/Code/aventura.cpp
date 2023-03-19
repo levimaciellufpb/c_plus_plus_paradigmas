3fa62c90ca9d10733a14acea90b4b58f8cbd94e6
levimaciell <levimaciell1221@gmail.com>
2023-03-19 15:12:50 -0300

    Atv01 feita

--- Atv01/Code/aventura.cpp
+++ Atv01/Code/aventura.cpp
@@ -2,44 +2,35 @@
 #include <string>
 
 using namespace std;
-//inputs
-//Nome da pessoa, nome de um amigo, idade da pessoa, idade de um amigo
+
 
 int main(){
 
-    int idadePessoa, idadeAmigo;
-    string nomePessoa, nomeAmigo;
+    const int OURO = 900;
+    unsigned int pessoas, mortos, sobreviventes;
 
-    cout << "Oi! bem vindos a aventura! Por favor, informe o seguinte para a sua aventura!" << endl;
+    cout << "Me diga um número: " << endl;
+    cin >> pessoas;
+    cout << "Me diga um número menor igual a este:" << endl;
+    cin >> mortos;
 
-    cout << "Qual o seu nome?" << endl;
-    cin >> nomePessoa;
+    while(mortos >= pessoas){
+        cout << "Você digitou um número maior que " << pessoas << "! Por favor, me diga um número menor:" << endl;
+        cin >> mortos;
+    }
 
-    cout << "Informe o nome de um amigo ou outra pessoa" << endl;
-    cin >> nomeAmigo;
+    sobreviventes = pessoas - mortos;
 
-    cout << "Agora, informe a idade dessa pessoa: ";
-    cin >> idadeAmigo;
+    cout << "O Naufrágio \n" << endl;
 
-    cout << "Por fim, informe a sua idade: " << endl;
-    cin >> idadePessoa;
+    cout << "Era uma noite fria em Vancouver, quando " << pessoas << " embarcaram em um navio que iria até Liverpool. " << endl;
 
-    cout << "   Era uma vez um ser mágico conhecido como Majin Boo. Ele era conhecido por sua aparência peculiar, com pele rosa e um rosto gordinho, mas também era temido por sua imensa força e habilidades mágicas." << endl;
+    cout << "No caminho para Liverpool, uma grande baleia atingiu o casco do navio. " << endl;
 
-    cout << "   Majin Boo passou anos aterrorizando o universo, destruindo planetas inteiros e espalhando o caos por onde quer que passasse. Ele era invencível e aparentemente indestrutível." << endl;
+    cout << "O acidente causou um naufrágio que matou " << mortos << " pessoas das " << pessoas << " que embarcaram originalmente." << endl;
 
-    cout << "   No entanto, um dia, uma equipe de guerreiros corajosos decidiu enfrentar Majin Boo e impedir que ele causasse mais destruição. Liderados pelos lendários guerreiros Goku e Vegeta, os guerreiros mais poderosos do planeta se prepararam para a batalha final contra o temido vilão." << endl;
+    cout << "Os " << sobreviventes << " restantes acabaram chegando em uma pequena ilha, onde seriam apenas resgatadas após dois meses do naufrágio.\n" << endl;
 
-    cout << "   A batalha foi épica e intensa, com Majin Boo usando suas habilidades mágicas de regeneração . Mas os guerreiros não desistiram, e lutaram com toda a sua força para tentar derrotar o monstro." << endl;
-
-    cout << "   Os guerreiros estavam exaustos e parecia que a batalha estava perdida. Majin boo era muito poderoso para os guerreiros da terra. Mas então, algo surpreendente aconteceu: Entraram no campo de batalha os guerreiros " << nomePessoa << " e " << nomeAmigo << " para enfrentar o temivel monstro rosa." << endl;
-
-    cout << "   Foi uma batalha difícil, mas majin boo estava levando a melhor sobre os dois poderosos guerreiros " << nomePessoa << " e " << nomeAmigo << ", eles não conseguiriam lutar por muito mais tempo." << endl;
-
-    //Mudar linha abaixo para o nome do personagem mais velho
-    cout << "   Então, em um ato de desespero, o guerreiro mais velho decide agarrar o monstro e usar toda sua energia para se auto-destruir e levar majin boo no processo. Quando a grande nuvem de poeira abaixou, o monstro não existia mais, assim como o guerreiro mais velho também foi destruido" << endl;
-
-    cout << "   Graças ao guerreiro mais velho, a terra viveria um futuro de paz e esperança!" << endl;
-
+    cout << "Fim :)" << endl;
 
 }
