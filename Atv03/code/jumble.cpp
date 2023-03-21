bcafe02fcd6d4a88c35e23e556169a8383117729
levimaciellufpb <levi.maciel@dcx.ufpb.br>
2023-03-21 13:00:28 -0300

    Bugged

---
+++ Atv03/code/jumble.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    const string PALAVRAS;
+    enum campos{PALAVRA, DICA, NUM_CAMPOS};
+
+    const int NUM_PALAVRAS = 5;
+    const string PALAVRAS [NUM_PALAVRAS][NUM_CAMPOS]= {
+        {"parede", "Você sente vontade de bater sua cabeça nela"},
+        {"óculos","Talvez lhe ajude a ver a resposta"},
+        {"",""},
+        {"",""},
+        {"",""}
+    };
+
+
+
+
+
+
+}
