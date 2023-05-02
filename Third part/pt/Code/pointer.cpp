25a018a9205d446d75d7375bb6e3a2ef34d6efdc
levimaciellufpb <levi.maciel@dcx.ufpb.br>
2023-05-02 10:33:04 -0300

    pointer.cpp feito

--- Third part/pt/Code/pointer.cpp
+++ Third part/pt/Code/pointer.cpp
@@ -15,14 +15,22 @@ int main(){
     //segundo ponteiro que vai apontar para p1
     shared_ptr<string> pointer2(pointer1);
 
-    //cout << 
+    cout << *pointer2 << endl; //Imprime estegossauro
+
+    shared_ptr<string> pointer3 = std::make_shared<string>("T-Rex");
+
+    pointer2 = pointer3;
+
+    cout << *pointer2 << endl; //Imprime T-Rex
 
 
+    if(pointer1)
+        cout << "Pointer1 não null? " << pointer1.use_count() << endl;
+        
+    if(pointer3)
+        cout << "Pointer3 não null? " << pointer3.use_count() << endl;
 
-
-
-
-
+    cout << *pointer2 << endl;
 
     return 0;
 }
